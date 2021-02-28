2aefce2a4b500d48b6c4b1322a66c76555e3e7cb
Ben Williams <benj@minwilliams.net>
2021-02-28 21:21:32 +1100

    add LED startup diag

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 
 import time
 import measure
+import lights
+
+def startup():
+    time.sleep(0.1)
+    lights.ten()
+    time.sleep(0.3)
+    lights.reset()
+    time.sleep(0.2)
+    lights.ten()
+    time.sleep(0.1)
+    lights.reset()
+    time.sleep(0.1)
+    lights.ten()
+    time.sleep(0.1)
+    ligths.reset()
+
+startup()
 
 while True:
     measure.checkDistance()
